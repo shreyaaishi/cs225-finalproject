fd2bf3df3442dbd665507c9818f75f867c74052f
Sikligar, Drashti <dsikli2@illinois.edu>
2021-04-24 16:26:10 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+class graph;
+struct node
+{
+    int nodeNumber
+    /* data */
+};
+struct edge{
+int startNode;
+int endNode;
+int weight; // find startNode edges and endNode edges, ^(-1)
+};
+
+void readFile{ // read file
+    vector<int> list;
+    ifstream infile;
+    infile.open("roadNet-PA.txt");
+    while(!infile.eof){
+        getline(infile, list);
+    }
+    infile.close;
+}
+//read roadNet
+//find all unique numbers, assign as nodes
+//read in connections from txt, find edges
+// thru public source -- cite, if its from this class, its okay except from prev sem labs
+//spec -- lab flow
+// parse, put into graph structure, traversal or algorithm done

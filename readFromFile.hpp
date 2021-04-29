593d868a658c63ffba07b58c3f97216b3b7b609f
Zhan, Elizabeth <ezhan292@illinois.edu>
2021-04-29 15:20:51 -0500

    Create readFromFile.hpp

---
+++ readFromFile.hpp
@@ -0,0 +1,15 @@
+#include <string>
+
+/** 
+ * Reads a file into a string
+ * @param filename The file to read from 
+ * @return a string containing filename's contents
+ */
+std::string file_to_string(const std::string & filename);
+
+/** 
+ * Reads a file into a vector
+ * @param filename The file to read from
+ * @return a vector containing filename's contents
+ */
+std::vector<std::string> file_to_vector(const std::string & filename);

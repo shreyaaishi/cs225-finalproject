d1811420e02165a819044cb3184c4ab6f078cdfe
Shreya Basu <shreyab4@siebl-0224-42.ews.illinois.edu>
2021-05-11 22:48:15 -0500

    Completed greedy graph coloring algorithm

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -7,12 +8,14 @@
 //#include "graph.h"
 
 int main() {
+    // Note: Include an extra line break after the list of edges
     Graph graph("PA.txt");
-    graph.printGraph();
-/*     std::vector<int> result = graph.BFS();
+    //graph.printGraph();
+     std::vector<int> result = graph.BFS();
     for (unsigned i = 0; i < result.size(); i++) {
         std::cout<<result[i] << " ";
-    } */
+    }
+    std::cout<<std::endl;
     graph.colorGraph();
     return 0;
 }

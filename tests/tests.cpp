44a3f69edb5693afdad9485cad3488b46fba00e3
Shreya Basu <shreyab4@siebl-0224-28.ews.illinois.edu>
2021-05-12 14:00:30 -0500

    Finished writing createGraph and BFS traversal tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,26 +1,56 @@
+#include <map>
 #include <string>
 #include <vector>
 
 #include "../catch/catch.hpp"
 #include "../graph.h"
 
-// createGraph tests
-TEST_CASE("All vertices added to graph") {
-    Graph graph("tests/sampleGraph1.txt");
+// Graph constructor tests
+TEST_CASE("All vertices added to graph only once - small") {
+    Graph graph("tests/smallGraph1.txt");
     std::vector<int> vertices = graph.getVertexList();
     REQUIRE(vertices.size() == 4);
 }
-
-//Test Cases Remaining:
-//No multi-edges added
-//No self-cycles added -- create graph tests
-//All edges added
-//No repeat vertices added
+TEST_CASE("All vertices added to graph only once -- large") {
+    Graph graph("tests/largeGraph.txt");
+    std::vector<int> vertices = graph.getVertexList();
+    REQUIRE(vertices.size() == 223);
+}
+TEST_CASE("addEdge adds the edge to both vertices") {
+    Graph graph("tests/smallGraph1.txt");
+    std::vector<int> zeroSol = {1, 2, 3};
+    std::vector<int> threeSol = {0, 2};
+    graph.addEdge(0, 3);
+    int i = 0;
+    for (std::pair<const int, double> & key_val : graph.getAdjacencyList(0)) {
+        REQUIRE(key_val.first == zeroSol[i]);
+        i++;
+    }
+    i = 0;
+    for (std::pair<const int, double> & key_val : graph.getAdjacencyList(3)) {
+        REQUIRE(key_val.first == threeSol[i]);
+        i++;
+    }
+}
+TEST_CASE("Multi-edges aren't added") {
+    Graph graph("tests/smallGraph1.txt");
+    std::map<int, double> prevAdjList0 = graph.getAdjacencyList(0);
+    std::map<int, double> prevAdjList2 = graph.getAdjacencyList(2);
+    graph.addEdge(2,0);
+    REQUIRE(prevAdjList0.size() == graph.getAdjacencyList(0).size());
+    REQUIRE(prevAdjList2.size() == graph.getAdjacencyList(2).size());
+}
+TEST_CASE("Self-cycles aren't created") {
+    Graph graph("tests/smallGraph1.txt");
+    std::map<int, double> prevAdjList = graph.getAdjacencyList(3);
+    graph.addEdge(3,3);
+    REQUIRE(prevAdjList.size() == graph.getAdjacencyList(3).size());
+}
 
 // BFS traversal tests
 TEST_CASE("BFS traversal reaches every node in BFS order") {
     std::vector<int> solution = {0,1,2,3};
-    Graph graph("tests/sampleGraph1.txt");
+    Graph graph("tests/smallGraph1.txt");
     std::vector<int> result = graph.BFS();
     for (unsigned i = 0; i<result.size(); i++) {
         REQUIRE(solution[i] == result[i]);
@@ -28,16 +58,15 @@ TEST_CASE("BFS traversal reaches every node in BFS order") {
 }
 TEST_CASE("BFS traversal reaches every node in a disconnected graph while maintaining BFS order") {
     std::vector<int> solution = {0, 4, 1, 2, 3};
-    Graph graph("tests/sampleGraph2.txt");
+    Graph graph("tests/smallGraph2.txt");
     std::vector<int> result = graph.BFS();
     for (unsigned i = 0; i<result.size(); i++) {
         REQUIRE(solution[i] == result[i]);
     }
 }
-//BFS traversal reaches every edge for connected graph
-//BFS traversal reaches every edge for a disconnected graph
-//BFS traversal is correct order
-//Note: some edge case isn't being handled when the first node isn't zero
+
+//graph coloring tests
+// no adjacent nodes have same color
 
 
 // dijkstra's tests
@@ -45,5 +74,3 @@ TEST_CASE("BFS traversal reaches every node in a disconnected graph while mainta
 // prioritize inner paths
 // weights on the edges prioritizes a non-obvious path (EX: A->C has weight 10, A->B->C has total weight 8) 
 
-//graph coloring tests
-// no adjacent nodes have same color
